576531b7bd37d07603dd47d6d96128e71128a406
unknown <t.hasan.prince@gmail.com>
2023-12-30 10:50:43 +0600

    [Ongoing]- Search Component

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,16 @@
 import HeroSection from "@/components/heroSection/HeroSection";
+import PageSearch from "@/components/pageSearch/PageSearch";
 
 const Home = () => {
-  return <>
-<HeroSection></HeroSection>
-  </>;
+  return (
+    <>
+      <HeroSection></HeroSection>
+      <PageSearch/>
+      {/* Featured Room */}
+      {/* Gallery */}
+      {/* News Letter */}
+    </>
+  );
 };
 
 export default Home;
