020d602b869537e35d0fd46b6491848a2c136a3d
unknown <t.hasan.prince@gmail.com>
2024-01-01 10:50:41 +0600

    Booking page done- Starting user page

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -73,8 +73,8 @@ export async function POST(req: Request, res: Response) {
       success_url: `${origin}/users/${userId}`,
       metadata: {
         adults,
-        checkinDate,
-        checkoutDate,
+        checkinDate: formattedCheckinDate,
+        checkoutDate: formattedCheckoutDate,
         children,
         hotelRoom: room._id,
         numberOfDays,
